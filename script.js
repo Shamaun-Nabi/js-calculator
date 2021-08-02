99d2a95901c7ccfac84de82c372cf9e5b1874551
Shamaun-Nabi <snashik950@gmail.com>
2021-08-02 21:15:38 +0600

    woking function

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ function getScreenValue() {
 }
 
 function screenOutput(num) {
-    return document.getElementById("screen").innerText = commaSeparate(num);
+    if (num == " ") {
+        document.getElementById("screen").innerText = num;
+    } else {
+        document.getElementById("screen").innerText = commaSeparate(num);
+    }
+
 }
 
 // Making Comma separated value
@@ -28,17 +33,36 @@ let takeValue;
 const allNumber = document.getElementsByClassName("number");
 for (let i = 0; i < allNumber.length; i++) {
     allNumber[i].addEventListener("click", function () {
-        takeValue=getHistory();
-        alert(takeValue);
-        takeValue+=+this.id;
+        takeValue = getHistory();
+        takeValue = takeValue + this.id;
+        printHistory(takeValue);
 
-        
     })
 }
+
 // taking operator
 const allOperator = document.getElementsByClassName("operator");
 for (let i = 0; i < allOperator.length; i++) {
     allOperator[i].addEventListener("click", function () {
-       
+        if (this.id == "clear") {
+            printHistory(" ");
+            screenOutput(" ");
+        } else if (this.id == "backspace") {
+            takeValue = getHistory()
+            takeValue = takeValue.substr(0, takeValue.length - 1);
+            printHistory(takeValue);
+            screenOutput(" ");
+        } else if (this.id == "equal") {
+            takeValue = getHistory();
+            let result = eval(takeValue);
+            screenOutput(result);
+            printHistory(takeValue);
+        } else {
+            takeValue = getHistory();
+            takeValue += this.id;
+            printHistory(takeValue);
+
+        }
+
     })
 }
